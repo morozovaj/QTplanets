a4c13b4aa66b21107d84a14a0beeb4bd30c6bf56
Opilane TTHK <opilane@tthk.ee>
2018-04-25 17:34:48 +0300

    2nd change. qDebug - hello

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QtDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    qDebug() << "HELLO" << endl;
 }
 
 MainWindow::~MainWindow()
